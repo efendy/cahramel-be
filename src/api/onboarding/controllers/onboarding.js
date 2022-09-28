7fd058294b83296c81b2a9bbeee46db935e4b149
efendygrowfi <efendysetiawan@growfi.ai>
2022-09-28 14:00:52 +0800

    remove onboarding collection

--- src/api/onboarding/controllers/onboarding.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * onboarding controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::onboarding.onboarding');
--- src/api/onboarding/routes/onboarding.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * onboarding router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::onboarding.onboarding');
