a00df2f06b815efecf5d647e5675e8f8a2cd48ca
efendygrowfi <efendysetiawan@growfi.ai>
2022-10-04 14:51:54 +0800

    gitignore

--- src/api/user-profile/controllers/user-profile.js
+++ src/api/user-profile/controllers/user-profile.js
@@ -6,4 +6,36 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::user-profile.user-profile');
+module.exports = createCoreController('api::user-profile.user-profile', ({ strapi }) =>  ({
+  // async create(ctx) {
+  //   let response = {
+  //     data: null,
+  //     error: {
+  //       status: 401,
+  //       name: "UnauthorizedError",
+  //       message: "Required valid authentication"
+  //     }
+  //   };
+
+  //   if (ctx.state?.user) {
+  //     const { id } = ctx.request.body.data;
+  //     const userEntry = await strapi.db.query("plugin::users-permissions.user").findOne({
+  //       where: { id },
+  //     });
+  //     const userMobile = userEntry.external_data.data.find(o => o.providerId === 'phone');
+  //     const userEmail = userEntry.external_data.data.find(o => o.providerId === 'email');
+
+  //     if (userMobile) {
+  //       console.log(userMobile);
+  //       ctx.request.body.data.phone_mobile = userMobile.uid;
+  //     }
+  //     if (userEmail) {
+  //       console.log(userEmail);
+  //       ctx.request.body.data.email = userEmail.uid;
+  //     }
+
+  //   }
+  //   return response;
+  // },
+
+}));
