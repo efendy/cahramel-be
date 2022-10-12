99c2f2bc0067e54fd4fab6b1661afd5ae8ab9148
efendygrowfi <efendysetiawan@growfi.ai>
2022-10-12 13:56:11 +0800

    update user access management

--- src/api/user-right/controllers/user-right.js
+++
@@ -1,7 +0,0 @@
-/**
- * user-right controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::user-right.user-right');
--- src/api/user-right/routes/user-right.js
+++
@@ -1,7 +0,0 @@
-/**
- * user-right router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::user-right.user-right');
