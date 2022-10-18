7a07300bbf62cad9790ff4933a6325a5b3875d58
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-10-18 09:01:32 +0800

    added /me

--- src/api/user-profile/controllers/user-profile.js
+++ src/api/user-profile/controllers/user-profile.js
@@ -6,4 +6,14 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::user-profile.user-profile');
+module.exports = createCoreController('api::user-profile.user-profile', () => ({
+  async findOne(ctx) {
+    if (ctx.params.id == "me") {
+      if (ctx.state?.user) {
+        ctx.params.id = ctx.state?.user?.id;
+      }
+    }
+    const response = await super.findOne(ctx);
+    return response;
+  },
+}));
