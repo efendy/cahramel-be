23f96ae5a6297cf4e4549fa4419143c1e52e9bb2
Kyaw Thura <44088165+kyawthura-gg@users.noreply.github.com>
2022-10-18 17:15:28 +0800

    fix /me

--- src/api/user-profile/controllers/user-profile.js
+++ src/api/user-profile/controllers/user-profile.js
@@ -6,15 +6,16 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::user-profile.user-profile', () => ({
+module.exports = createCoreController('api::user-profile.user-profile', ({ strapi }) => ({
   async findOne(ctx) {
     let response = null;
     if (ctx.params.id == "me" && ctx.state?.user?.id) {
+      console.log(ctx.state?.user?.id)
       const userId = ctx.state?.user?.id;
 
       delete ctx.params.id;
 
-      ctx.request.url += `&filters[user][$eq]=${userId}`;
+      ctx.request.url = `/api/user-profiles?filters[users]=${userId}`;
 
       const result = await super.find(ctx);
       if (result.data?.length > 0) {
