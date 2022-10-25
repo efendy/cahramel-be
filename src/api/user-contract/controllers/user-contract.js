7c4ad9e84dae869dc5d727de39fc73420aefe77c
efendygrowfi <efendysetiawan@growfi.ai>
2022-10-25 16:56:35 +0800

    error handling

--- src/api/user-contract/controllers/user-contract.js
+++ src/api/user-contract/controllers/user-contract.js
@@ -3,59 +3,105 @@
 /**
  * user-contract controller
  */
+ const utils = require('@strapi/utils');
+ const { NotFoundError, ForbiddenError } = utils.errors;
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::user-contract.user-contract', ({ strapi }) => ({
 
   async draftSave(ctx) {
-    let response = {
-      data: null,
-      error: {
-        status: 400,
-        name: "Bad Request",
-        message: "Invalid Request"
-      }
-    };
+    let response;
 
+    const userId = ctx.state?.user?.id;
+    console.log(userId);
+
+    if (userId && ctx.request.body?.data) {
+      const { id, profile, contract } = ctx.request.body.data;
+      
+      if (profile && contract) {
+        // retrieve authenticated user profile for getting user contract
+        const authUserProfile = await strapi.db.query("api::user-profile.user-profile").findOne({
+          select: ['id'],
+          where: { users: userId },
+        });
+
+        // retrieve authenticated user contract for access_role and company ids
+        // to ensure the authenticated user has the permission to set
+        const authUserContracts = await strapi.db.query("api::user-contract.user-contract").findMany({
+          select: ['id', 'access_role'],
+          where: { user_profile: authUserProfile.id },
+          populate: {
+            company_profile: {
+              select: ['id']
+            }
+          },
+        });
+
+        // an admin doesn't allow to add user to other company
+        const authUserCompanyIds = authUserContracts.map(object => object.company_profile.id);
+        if (!authUserCompanyIds.includes(contract.company_id)) {
+          console.log(`auth user does not contract with the company id ${contract.company_id}.`);
+          throw new ForbiddenError('You didn\'t say the magic word');
+        }
+
+        // an admin doesn't allow to set access_role owner for other user.
+        // const authUser
+        if (contract.access_role === 'owner' && authUserContracts.access_role === 'owner') {
+          throw new ForbiddenError('You didn\'t say the magic word');
+        }
+        if (authUserContracts.access_role === 'admin' && contract.access_role === 'owner') {
+          throw new ForbiddenError('You didn\'t say the magic word');
+        }
+
+        throw new ForbiddenError('You didn\'t say the magic word');
+
+        let userContract;
+        if (id) {
+          userContract = await strapi.db.query("api::user-contract.user-contract").findOne({
+            select: ['id'],
+            where: { id },
+          });
+        }
+
+        // retrieve existing user-profile by email
+        const userProfile = await strapi.db.query("api::user-profile.user-profile").findOne({
+          select: ['id', 'first_name', 'last_name', 'email_address', 'phone_number'],
+          where: { email_address: profile.email },
+        });
+
+        if (userProfile) {
+
+        }
+      }
+    }
+
+    if (!response)  {
+      throw new NotFoundError('Does not exist');
+    }
     return response;
   },
 
   async draftDelete(ctx) {
-    let response = {
-      data: null,
-      error: {
-        status: 400,
-        name: "Bad Request",
-        message: "Invalid Request"
-      }
-    };
+    let response;
 
+    if (!response)  {
+      throw new NotFoundError('Does not exist');
+    }
     return response;
   },
 
   async draftConfirm(ctx) {
-    let response = {
-      data: null,
-      error: {
-        status: 400,
-        name: "Bad Request",
-        message: "Invalid Request"
-      }
-    };
+    let response;
 
+    if (!response)  {
+      throw new NotFoundError('Does not exist');
+    }
     return response;
   },
 
   async inviteCheck(ctx) {
-    let response = {
-      data: null,
-      error: {
-        status: 400,
-        name: "Bad Request",
-        message: "Invalid Request"
-      }
-    };
+    let response;
 
     if (ctx.params.code) {
       // retrieve user contract using invitation code to get the user profile id
@@ -85,18 +131,14 @@ module.exports = createCoreController('api::user-contract.user-contract', ({ str
       }
     }
 
+    if (!response)  {
+      throw new NotFoundError('Does not exist');
+    }
     return response;
   },
 
   async linkUser(ctx) {
-    let response = {
-      data: null,
-      error: {
-        status: 400,
-        name: "Bad Request",
-        message: "Invalid Request"
-      }
-    };
+    let response;
 
     const userId = ctx.state?.user?.id;
 
@@ -175,6 +217,10 @@ module.exports = createCoreController('api::user-contract.user-contract', ({ str
           response = this.transformResponse(updateUserProfile);
         }
       }
+    }
+    
+    if (!response)  {
+      throw new NotFoundError('Does not exist');
     }
     return response;
   },
