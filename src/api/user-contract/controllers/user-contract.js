20677c8ab8d4fb39d8495d91ca654ebb89cca625
efendygrowfi <efendysetiawan@growfi.ai>
2022-10-25 09:54:31 +0800

    update check invite impacted by schema changed

--- src/api/user-contract/controllers/user-contract.js
+++ src/api/user-contract/controllers/user-contract.js
@@ -58,6 +58,68 @@ module.exports = createCoreController('api::user-contract.user-contract', ({ str
     };
 
     if (ctx.params.code) {
+      const userContract = await strapi.db.query("api::user-contract.user-contract").findOne({
+        select: ['id'],
+        where: { code: ctx.params.code },
+        populate: {
+          user_profile: {
+            select: ['id'],
+          },
+        },
+      });
+
+      if (userContract?.user_profile?.id) {
+        const userProfile = await strapi.db.query("api::user-profile.user-profile").findOne({
+          select: ['id', 'first_name', 'last_name', 'email_address', 'phone_number'],
+          where: { id: userContract.user_profile.id },
+          populate: {
+            users: {
+              select: ['id', 'email'],
+            },
+          },
+        });
+
+        response = this.transformResponse(userProfile);
+      }
+    }
+
+    return response;
+  },
+
+  async linkUser(ctx) {
+    let response = {
+      data: null,
+      error: {
+        status: 400,
+        name: "Bad Request",
+        message: "Invalid Request"
+      }
+    };
+
+    const userId = ctx.state?.user?.id;
+
+    if (userId && ctx.request.body?.data) {
+      const { code, byemail } = ctx.request.body.data;
+
+      if (byemail) {
+
+      } else {
+        if (code) {
+          const entity = await strapi.db.query("api::user-contract.user-contract").findOne({
+            select: ['id'],
+            where: { code },
+            populate: {
+              user_profile: {
+                select: ['id', 'email_address'],
+              },
+            },
+          });
+
+          // if 
+        }
+      }
+    }
+    if (ctx.params.code) {
       const entity = await strapi.db.query("api::user-contract.user-contract").findOne({
         select: ['id'],
         where: { code: ctx.params.code },
@@ -74,19 +136,6 @@ module.exports = createCoreController('api::user-contract.user-contract', ({ str
       const sanitizedEntity = await this.sanitizeOutput(entity);
       response = this.transformResponse(entity);
     }
-
-    return response;
-  },
-
-  async linkUser(ctx) {
-    let response = {
-      data: null,
-      error: {
-        status: 400,
-        name: "Bad Request",
-        message: "Invalid Request"
-      }
-    };
 
     return response;
   },
