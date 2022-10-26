b9b62b15f874bfa56e3e8886ca07c0ac1572b57d
efendygrowfi <efendysetiawan@growfi.ai>
2022-10-26 09:00:50 +0800

    save draft ready

--- src/api/user-contract/controllers/user-contract.js
+++ src/api/user-contract/controllers/user-contract.js
@@ -3,8 +3,11 @@
 /**
  * user-contract controller
  */
- const utils = require('@strapi/utils');
- const { NotFoundError, ForbiddenError } = utils.errors;
+const utils = require('@strapi/utils');
+const dayjs = require('dayjs');
+const customParseFormat = require('dayjs/plugin/customParseFormat')
+dayjs.extend(customParseFormat)
+const { NotFoundError, ForbiddenError } = utils.errors;
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
@@ -14,7 +17,6 @@ module.exports = createCoreController('api::user-contract.user-contract', ({ str
     let response;
 
     const userId = ctx.state?.user?.id;
-    console.log(userId);
 
     if (userId && ctx.request.body?.data) {
       const { id, profile, contract } = ctx.request.body.data;
@@ -27,7 +29,7 @@ module.exports = createCoreController('api::user-contract.user-contract', ({ str
         });
 
         // retrieve authenticated user contract for access_role and company ids
-        // to ensure the authenticated user has the permission to set
+        // to ensure the authenticated user has the permission to set.
         const authUserContracts = await strapi.db.query("api::user-contract.user-contract").findMany({
           select: ['id', 'access_role'],
           where: { user_profile: authUserProfile.id },
@@ -38,41 +40,113 @@ module.exports = createCoreController('api::user-contract.user-contract', ({ str
           },
         });
 
-        // an admin doesn't allow to add user to other company
+        // an authenticated user doesn't allow to create user for different company
+        // which the authenticated user doesn't have contract to.
         const authUserCompanyIds = authUserContracts.map(object => object.company_profile.id);
         if (!authUserCompanyIds.includes(contract.company_id)) {
           console.log(`auth user does not contract with the company id ${contract.company_id}.`);
           throw new ForbiddenError('You didn\'t say the magic word');
         }
 
-        // an admin doesn't allow to set access_role owner for other user.
-        // const authUser
-        if (contract.access_role === 'owner' && authUserContracts.access_role === 'owner') {
-          throw new ForbiddenError('You didn\'t say the magic word');
-        }
-        if (authUserContracts.access_role === 'admin' && contract.access_role === 'owner') {
+        // validate setting access_role
+        const authUserAccessRole = authUserContracts.find(object => object.company_profile.id === contract.company_id)?.access_role;
+        if ((contract.access_role === 'owner' && (authUserAccessRole === 'user' || authUserAccessRole === 'admin')) ||
+            authUserAccessRole === 'user') {
           throw new ForbiddenError('You didn\'t say the magic word');
         }
 
-        throw new ForbiddenError('You didn\'t say the magic word');
+        let userProfileId;
+        // check if user profile exists.
+        let userProfile = await strapi.db.query("api::user-profile.user-profile").findOne({
+          select: ['id', 'first_name', 'last_name', 'email_address', 'phone_number'],
+          where: { email_address: profile.email },
+        });
+        
+        if (userProfile) {
+          console.log('userProfile', userProfile);
+          userProfileId = userProfile.id;
+        } else {
+          // user-profile CREATION
+          const createUserProfile = await strapi.db.query("api::user-profile.user-profile").create({
+            select: ['id', 'first_name', 'last_name', 'email_address', 'phone_number'],
+            data: {
+              first_name: profile.first_name,
+              last_name: profile.last_name || "",
+              email_address: profile.email,
+              country_code: profile.country_code || null,
+              phone_number: profile.phone_number || null
+            }
+          });
+          console.log('createUserProfile', createUserProfile);
 
+          userProfile = createUserProfile;
+          userProfileId = createUserProfile.id;
+        }
+
+        // user-contract creation / update
+        let userContractId;
         let userContract;
+        // check if user contract exists.
         if (id) {
           userContract = await strapi.db.query("api::user-contract.user-contract").findOne({
             select: ['id'],
             where: { id },
           });
+          if (!userContract) {
+            throw new NotFoundError('Does not exist');
+          }
+          userContractId = userContract.id;
         }
 
-        // retrieve existing user-profile by email
-        const userProfile = await strapi.db.query("api::user-profile.user-profile").findOne({
-          select: ['id', 'first_name', 'last_name', 'email_address', 'phone_number'],
-          where: { email_address: profile.email },
+        const userContractData = {
+          employee_id: contract.employee_id,
+          job_title: contract.job_title_id,
+          reporting_to_main: contract.reporting_to_main_id,
+          reporting_to_secondary: contract.reporting_to_secondary_id,
+          department: contract.department_id,
+          company_profile: contract.company_id,
+          date_start: contract.date_start ? dayjs(contract.date_start).format('YYYY-MM-DD') : null,
+          date_end: contract.date_end ? dayjs(contract.date_end).format('YYYY-MM-DD') : null,
+          onboarding_status: contract.onboarding_status,
+          offboarding_status: contract.offboarding_status,
+          email_address: contract.email,
+          access_role: contract.access_role,
+          user_profile: userProfileId
+        };
+
+        if (userContractId) {
+          // UPDATE user contract
+          const updateUserContract = await strapi.db.query("api::user-contract.user-contract").update({
+            where: { id: userContractId },
+            data: userContractData
+          });
+
+          if (!updateUserContract) {
+            throw new NotFoundError('DB Update Contract');
+          }
+
+          userContract = updateUserContract;
+        } else {
+          // CREATE user contract
+          const createUserContract = await strapi.db.query("api::user-contract.user-contract").create({
+            data: userContractData,
+          });
+
+          if (!createUserContract) {
+            throw new NotFoundError('DB Create Contract');
+          }
+
+          userContract = createUserContract;
+        }
+
+        delete userContract.createdAt;
+        delete userContract.updatedAt;
+        delete userContract.code;
+
+        response = this.transformResponse({
+          profile: userProfile,
+          contract: userContract,
         });
-
-        if (userProfile) {
-
-        }
       }
     }
 
@@ -218,7 +292,7 @@ module.exports = createCoreController('api::user-contract.user-contract', ({ str
         }
       }
     }
-    
+
     if (!response)  {
       throw new NotFoundError('Does not exist');
     }
